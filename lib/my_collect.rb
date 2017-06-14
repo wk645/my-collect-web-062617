3fbd6d874288ac1acbd3f1ebed26273575603d26
wk645 <wk645@nyu.edu>
2017-06-14 18:11:09 -0400

    collect lab

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
+def my_collect(collection)
+    i = 0
+    empty_array = []
+    
+    while i < collection.length
+        empty_array << (yield(collection[i]))
+        i += 1
+    end
 
+    empty_array
+end
 
